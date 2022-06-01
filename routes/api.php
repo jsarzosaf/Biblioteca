14b56e2356ad0d9029038c5914bf06244bc48f55
Jose Sarzosa <jsarzosa@sipecom.com>
2022-06-01 17:20:27 -0500

    Se agregaron los endpoints solicitados

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BibliotecaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,32 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Ruta para listar los libros activos de la biblioteca
+ * 
+ * Si se le proporciona el parametro opcional etiqueta y value
+ * se indica que se hara una busqueda por etiquetas ambos deben
+ * especificar para que de una busqueda exitosa, sino se indica
+ * ninguno de los parametros opcionales se retornara todos los 
+ * libros disponibles 
+ */
+Route::get('listar_libros/{etiqueta?}/{value?}', [BibliotecaController::class, 'getLibros']);
+
+/**
+ * Ruta para poder prestar un libro, donde se debe especificar los siguiente parametros
+ * el identificador del libro (libro_id), la fecha de entrega que devolvera el libro,
+ * y el nombre del usuario que registra solicitud y el nombre del usuario solicitante
+ */
+Route::post('prestar_libro', [BibliotecaController::class, 'registerPrestamo']);
+
+/**
+ * Ruta para dar de altas libros en el sistema de la biblioteca, se debe especificar el identificador
+ * del libro (libro_id), la cantidad de ejemplares de dar de alta, y el motivo de la alta que puede ser 
+ * (migracion, donacion, compra, daño)
+ */
+Route::post('dar_alta_libro', [BibliotecaController::class, 'addStockLibro']);
+
+
+
+
